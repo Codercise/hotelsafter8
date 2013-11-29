5c23473b971de1932fdcc739f141c374c9fd8f5b
Michael Dawson <m.dawson@y7mail.com>
2013-11-30 00:12:30 +1030

    Javascript work

---
+++ app/assets/javascripts/lib/location.js
@@ -0,0 +1,7 @@
+
+navigator.geolocation.getCurrentPosition(GetLocation);
+function GetLocation(location) {
+    alert(location.coords.latitude);
+    alert(location.coords.longitude);
+    alert(location.coords.accuracy);
+}
